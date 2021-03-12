7c18cfd7ec1384aae9d311711ed1ed2c51d089a7
Martin Beeby <martinbeeby@hotmail.com>
2021-03-12 01:35:09 +0000

    Fixing Application

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/<random_string>')
 def returnBackwardsString(random_string):
     """Reverse and return the provided URI"""
-    return "Breaking the Unit test"
+    return "".join(reversed(random_string))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)

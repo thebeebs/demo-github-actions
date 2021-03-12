ce69390d0a8f694d4706f7a503a203aee9bdd8de
Martin Beeby <martinbeeby@hotmail.com>
2021-03-12 00:46:17 +0000

    Commiting change to break the unit test

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/<random_string>')
 def returnBackwardsString(random_string):
     """Reverse and return the provided URI"""
-    return "".join(reversed(random_string))
+    return "Breaking the Unit test"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
